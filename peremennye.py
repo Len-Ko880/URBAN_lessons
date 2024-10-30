4e53b2f9143f4b6c7cd1100e7a4e4def12fc5a0c
Helena <165100850+Len-Ko880@users.noreply.github.com>
2024-10-30 16:41:45 +0300

    Add files via upload

---
+++ peremennye.py
@@ -0,0 +1,5 @@
+count_home_work = 12
+hours = 1.5
+modul_name = 'Python'
+one_lesson_time = hours / count_home_work
+print('Курс:', modul_name, ',всего задач:', count_home_work, ',затрачено часов:', hours, ',среднее время выполнения:', one_lesson_time, 'часа.')
